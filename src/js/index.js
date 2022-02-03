d81c3931e6712847cb4853dabbb40d8e1b14cd48
Piotr Mazurek <mazurekp93@gmail.com>
2022-02-03 22:09:12 +0100

    hero section & hamburger menu styling

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,20 @@ import '../scss/main.scss';
 
 
 console.log('HELLO 🚀')
+const hamburger = document.querySelector('.burger')
+const navigation = document.querySelector('.navigation')
+const heroShapes = document.querySelectorAll('.hero__shape')
+setInterval(() => {
+
+    heroShapes.forEach(shape => {
+        shape.style.borderTopLeftRadius = `${Math.floor(Math.random() * 100)+40}%`;
+        shape.style.borderTopRightRadius = `${Math.floor(Math.random() * 100)+40}%`;
+        shape.style.borderBottomLeftRadius = `${Math.floor(Math.random() * 100)+40}%`;
+        shape.style.borderBottomRightRadius = `${Math.floor(Math.random() * 100)+40}%`;
+    })
+}, 4000)
+
+hamburger.addEventListener('click', () => {
+navigation.classList.toggle('navigation--open')
+hamburger.classList.toggle('open')
+})
